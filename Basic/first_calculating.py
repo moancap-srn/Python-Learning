9e7684294c3ac49cdf7e3f3fc2e806572bdab501
Roman <t2c2hfrggj@privaterelay.appleid.com>
2025-10-14 08:36:50 +0300

    learning-1
    [DAY 1]

---
+++ Basic/first_calculating.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = input()
+
+if c == "+":
+    print(a + b)
+elif c == "-":
+    print(a - b)
+elif c == "*":
+    print(a * b)
+elif c == "/":
+    if b == 0:
+        print("На ноль делить нельзя!")
+    else:
+        print(a / b)
+else:
+    print("Неверная операция, попробуйте ещё раз!")
